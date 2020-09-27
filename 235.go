07082c266ce53d381f229b540ac1471fbf8c00bc
gswlee <995030368@qq.com>
2020-09-27 18:29:12 +0800

    二叉搜索树的最近公共祖先

---
+++ 235.go
@@ -0,0 +1,9 @@
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root.Val>p.Val&&root.Val>q.Val{
+        return lowestCommonAncestor(root.Left,p,q)
+    }else if root.Val<p.Val&&root.Val<q.Val{
+        return lowestCommonAncestor(root.Right,p,q)
+    }else{
+        return root
+    }
+}
