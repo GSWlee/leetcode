75140f57752ad395ad9d7131b824b91f8241a6d1
gswlee <995030368@qq.com>
2022-01-02 16:41:40 +0800

    283. 移动零

--- 283.go
+++ 283.go
@@ -16,3 +16,16 @@ func moveZeroes(nums []int)  {
         }
     }
 }
+
+func moveZeroes(nums []int)  {
+    i:=0
+    for _,v:=range nums{
+        if v!=0{
+            nums[i]=v
+            i++
+        }
+    }
+    for ;i<len(nums);i++{
+        nums[i]=0
+    }
+}
